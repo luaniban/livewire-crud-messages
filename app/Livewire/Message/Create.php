d3cb061e98968396caf2d2a1e0b4dbf5a0212e44
Murilo Francisco Matos <murilofm.social@gmail.com>
2024-12-16 19:06:06 +0000

    create message users table

--- app/Livewire/Message/Create.php
+++ app/Livewire/Message/Create.php
@@ -2,13 +2,41 @@
 
 namespace App\Livewire\Message;
 
+use App\Models\Message;
+use App\Models\User;
+
 use Livewire\Component;
 
 class Create extends Component
 {
     public $message;
+    public $isCargoSelected = false;
+    public $users;
+    public $destinatarios = [];
+    public $cargo;
+    public $destinatario;
+
     public function create() {
-        dump($this->message);
+        Message::create([
+            'message' => $this->message,
+            'destinatario' => $this->destinatario
+        ]);
+    }
+
+
+    public function cargoState() {
+        $this->destinatarios = [];
+        $this->isCargoSelected = true;
+        if($this->cargo == 'todos') {
+            $users = User::whereNot('cargo_id', 1)->get();
+        }
+        else {
+            $users = User::where('cargo_id', $this->cargo)->get();
+        }
+        $this->users = $users;
+        foreach ($users as $user) {
+            array_push($this->destinatarios, ['label' => $user->name, 'value' => $user->id]);
+        }
     }
 
     public function render()
--- resources/views/livewire/message/create.blade.php
+++ resources/views/livewire/message/create.blade.php
@@ -1,7 +1,16 @@
 <div>
     <div class="fixed inset-0 bg-gray-500 bg-opacity-50 flex justify-center items-center z-50">
-        <div class="bg-white p-6 rounded-lg shadow-lg w-96">
+        <div class="bg-white p-6 rounded-lg shadow-lg w-1/2">
             <h2 class="text-xl mb-4">Criar Nova Mensagem</h2>
+            <div class="flex items-center mb-4  space-x-2">
+                <x-ts-select.styled wire:model.live="cargo" wire:change="cargoState" label="Cargo" select="label:label|value:value" :options="[
+                    ['label' => 'Todos', 'value' => 'todos'],
+                    ['label' => 'Professor', 'value' => '2'],
+                    ['label' => 'Gestor', 'value' => '3'],
+                ]" />
+                <x-ts-select.styled wire:model.prevent="destinatario" label="Destinatário" select="label:label|value:value" :options="$destinatarios" multiple/>
+
+            </div>
 
             <x-ts-textarea wire:model="message" placeholder="Digite sua mensagem" label="Mensagem"></x-ts-textarea>
 
